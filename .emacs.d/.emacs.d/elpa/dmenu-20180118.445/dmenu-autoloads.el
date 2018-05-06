4e4f67d952177192f5b3e2f25d7ce46d18d2689f
Alekseev Artem <a.artem060@gmail.com>
2018-05-06 23:58:43 +0300

    fix .emacs.d

--- .emacs.d/.emacs.d/elpa/dmenu-20180118.445/dmenu-autoloads.el
+++
@@ -1,21 +0,0 @@
-;;; dmenu-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "dmenu" "dmenu.el" (23279 18100 663073 472000))
-;;; Generated autoloads from dmenu.el
-
-(autoload 'dmenu "dmenu" "\
-
-
-\(fn &optional PREFIX)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dmenu-autoloads.el ends here
--- .emacs.d/.emacs.d/elpa/popup-kill-ring-20131020.1154/popup-kill-ring-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "popup-kill-ring" "20131020.1154" "interactively insert item from kill-ring" '((popup "0.4") (pos-tip "0.4")) :commit "5773dfadc104a906c088a3ec62e8cdd3e01e57fa" :keywords '("popup" "kill-ring" "pos-tip") :url "https://github.com/waymondo/popup-kill-ring")
